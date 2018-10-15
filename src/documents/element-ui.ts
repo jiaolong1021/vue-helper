7b77fc0a8705bcb0b4b0b5002648b1de23beef0d
jiaolong1021 <shenjiaolong1021@163.com>
2018-10-15 23:06:43 +0800

    element docuement

--- src/documents/element-ui.ts
+++ src/documents/element-ui.ts
@@ -1,3 +1,27 @@
 export default {
-  
+  "el-row": `[element：http://element-cn.eleme.io/#/zh-CN/component/layout](http://element-cn.eleme.io/#/zh-CN/component/layout) \n
+  | props | 说明 | 类型 | 可选值 | 默认值 |
+| :--- | :--- | :--- | :--- |
+| gutter | 栅格间隔 | number | — | 0 |
+| type | 布局模式，可选 flex，现代浏览器下有效 | string | — | — |
+| justify | flex 布局下的水平排列方式 | string | start/end/center/space-around/space-between | start |
+| align | flex 布局下的垂直排列方式 | string | top/middle/bottom | top |
+| tag | 自定义元素标签 | string | * | div |
+  `,
+  "el-col": `[element：http://element-cn.eleme.io/#/zh-CN/component/layout](http://element-cn.eleme.io/#/zh-CN/component/layout) \n
+| props | 说明 | 类型 | 可选值 | 默认值 |
+| :--- | :--- | :--- | :--- |
+| span | 栅格占据的列数 | number | — | 24 |
+| offset | 栅格左侧的间隔格数 | number | — | 0 |
+| push | 栅格向右移动格数 | number | — | 0 |
+| pull | 栅格向左移动格数 | number | — | 0 |
+| xs | <768px 响应式栅格数或者栅格属性对象 | number/object (例如： {span: 4, offset: 4}) | — | — |
+| sm | ≥768px 响应式栅格数或者栅格属性对象 | number/object (例如： {span: 4, offset: 4}) | — | — |
+| md | ≥992px 响应式栅格数或者栅格属性对象 | number/object (例如： {span: 4, offset: 4}) | — | — |
+| lg | ≥1200px 响应式栅格数或者栅格属性对象 | number/object (例如： {span: 4, offset: 4}) | — | — |
+| xl | ≥1920px 响应式栅格数或者栅格属性对象 | number/object (例如： {span: 4, offset: 4}) | — | — |
+| tag | 自定义元素标签 | string | * | div |
+  `,
+  "Row": `[element：http://element-cn.eleme.io/#/zh-CN/component/layout](http://element-cn.eleme.io/#/zh-CN/component/layout) \n
+  `,
 }
